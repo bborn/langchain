a46f24ee347e14c7d95826e59b1aa3663d586cce
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2023-02-17 21:19:21 -0600

    use restricted python for PythonRepl exec

--- langchain/python.py
+++ langchain/python.py
@@ -2,22 +2,47 @@
 import sys
 from io import StringIO
 from typing import Dict, Optional
+from RestrictedPython import compile_restricted, utility_builtins, safe_builtins, limited_builtins
+from RestrictedPython.PrintCollector import PrintCollector
+_print_ = PrintCollector
+_getattr_ = getattr
 
-from pydantic import BaseModel, Field
+def default_guarded_getitem(ob, index):
+    return ob[index]
 
 
 class PythonREPL(BaseModel):
     """Simulates a standalone Python REPL."""
 
-    globals: Optional[Dict] = Field(default_factory=dict, alias="_globals")
-    locals: Optional[Dict] = Field(default_factory=dict, alias="_locals")
+    def __init__(self, _globals: Optional[Dict] = None, _locals: Optional[Dict] = None):
+        """Initialize with optional globals and locals."""
+        self._globals = _globals if _globals is not None else {}
+        self._locals = _locals if _locals is not None else {}
+
 
     def run(self, command: str) -> str:
         """Run command with own globals/locals and returns anything printed."""
         old_stdout = sys.stdout
         sys.stdout = mystdout = StringIO()
         try:
-            exec(command, self.globals, self.locals)
+            _print_ = PrintCollector
+            _getattr_ = getattr
+
+            globals = {
+                '__builtins__': safe_builtins,
+                "str": str,
+                "dict": dict,
+                "_print_": _print_,
+                "_getattr_": _getattr_,
+                "_getitem_": default_guarded_getitem
+                }
+            globals = globals.update(self._globals)
+
+            byte_code = compile_restricted(command, filename='<inline code>', mode='exec')
+
+            exec(byte_code, globals, self._locals)
+
+            # exec(command, self._globals, self._locals)
             sys.stdout = old_stdout
             output = mystdout.getvalue()
         except Exception as e:
